48751b8d68034858afc52635facc2faae77dd900
Lucas Godoy <godoy.lucas.e@gmail.com>
2017-11-13 09:31:39 +1300

    Initial project set up

---
+++ src/main/java/com/devit/checkmein/CheckMeInApplication.java
@@ -0,0 +1,12 @@
+package com.devit.checkmein;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheckMeInApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CheckMeInApplication.class, args);
+	}
+}
